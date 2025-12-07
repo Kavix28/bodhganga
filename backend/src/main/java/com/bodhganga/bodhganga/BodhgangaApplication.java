3aa892cdafcfc9a79f512a7fa34a3ad000149c27
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-07 18:33:46 +0530

    first commit

---
+++ backend/src/main/java/com/bodhganga/bodhganga/BodhgangaApplication.java
@@ -0,0 +1,13 @@
+package com.bodhganga.bodhganga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodhgangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodhgangaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bodhganga/bodhganga/BodhgangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodhganga.bodhganga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodhgangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
