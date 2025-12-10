d6f3a8005d4b1f015c0abd9d53998230405d5f21
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-10 13:02:36 +0530

    basic configurations

---
+++ backend/src/main/java/com/bodhganga/bodhganga/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.bodhganga.bodhganga.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/profile")
+public class UserController
+{
+
+}
---
+++ backend/src/main/java/com/bodhganga/bodhganga/entity/Courses.java
@@ -0,0 +1,52 @@
+package com.bodhganga.bodhganga.entity;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "courses")
+public class Courses
+{
+    @Id
+    @Indexed(unique = true)
+    @NonNull
+    private String id; //publicly exposed uuid
+
+    @Indexed(unique = true)
+    @NonNull
+    private String courseTitle;
+
+    @NonNull
+    public String description;
+
+    @NotNull
+    private String instructorName;
+
+    @NotNull
+    private String courseCategory;
+
+    @NotNull
+    private double courseDuration;
+
+    @NotNull
+    private double coursePrice;
+
+    private String thumbnailUrl;
+    private String language;
+    private Integer totalLectures;
+    private Double rating;
+    private Integer enrolledStudents;
+    private Date createdAt;
+    private Date updatedAt;
+
+}
