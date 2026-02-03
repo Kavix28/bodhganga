ccca8e54572ae8ce1b25353a811e1ca623c5089e
codes-by-utkarsh <usrivastava2023@gmail.com>
2026-02-04 01:52:37 +0530

    No major changes

--- backend/src/main/java/com/bodhganga/bodhganga/repo/userRepo.java
+++ backend/src/main/java/com/bodhganga/bodhganga/repo/userRepo.java
@@ -1,14 +1,14 @@
 package com.bodhganga.bodhganga.repo;
 
-import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bodhganga.bodhganga.entity.User;
 
+import java.util.Optional;
+
 @Repository
-interface UserRepo extends MongoRepository<User, String> {
+public interface UserRepo extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByPhoneNo(String phoneNo);
