10f31e7291511b2ed919f2ced77040f6137fbc62
codes-by-utkarsh <usrivastava2023@gmail.com>
2026-02-04 01:51:18 +0530

    DTO object where the signup is done

--- backend/src/main/java/com/bodhganga/bodhganga/dto/SignupRequestDTO.java
+++ backend/src/main/java/com/bodhganga/bodhganga/dto/SignupRequestDTO.java
@@ -2,13 +2,14 @@ package com.bodhganga.bodhganga.dto;
 
 import lombok.Data;
 import jakarta.validation.constraints.*;
+import java.util.Date;
 
 @Data
 public class SignupRequestDTO {
     @NotBlank(message = "Name is Required")
     private String name;
 
-    @NotBlank(message = "Name is Required")
+    @NotBlank(message = "Email is Required")
     @Email(message = "Invalid Email Format")
     private String email;
 
@@ -19,6 +20,12 @@ public class SignupRequestDTO {
     @NotBlank(message = "Password is required")
     @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotBlank(message = "Gender is required")
+    private String gender;
+
+    @NotNull(message = "Date of birth is required")
+    private Date dateOfBirth;
 
     @NotBlank(message = "City is required")
     private String city;
