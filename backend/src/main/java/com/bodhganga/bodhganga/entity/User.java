409e57e343323ccd5009faa710d79bdfbd6fd710
codes-by-utkarsh <usrivastava2023@gmail.com>
2026-02-04 00:18:51 +0530

    fixed NonNull fields

--- backend/src/main/java/com/bodhganga/bodhganga/entity/User.java
+++ backend/src/main/java/com/bodhganga/bodhganga/entity/User.java
@@ -4,7 +4,6 @@ import lombok.*;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,25 +19,38 @@ import java.util.List;
 public class User
 {
     @NonNull
-    private String fName;
-    @NonNull
-    private String lName;
-    @NonNull
     private String name;
+    public @NonNull String getName() {return name;}
+    public void setName(@NonNull String name) {this.name = name;}
+
     private String gender;
+
     @NonNull
     private Date dateOfBirth;
+    public @NonNull Date getDateOfBirth() {return dateOfBirth;}
+    public void setDateOfBirth(@NonNull Date dateOfBirth) {this.dateOfBirth = dateOfBirth;}
+
+
     @Indexed(unique = true)
     @NonNull
     private String email;
+    public @NonNull String getEmail() {return email;}
+    public void setEmail(@NonNull String email) {this.email = email;}
+
+
     private String hashedPassword;
     @Indexed(unique = true)
     @NonNull
+
     private String phoneNo;
+    public @NonNull String getPhoneNo() {return phoneNo;}
+    public void setPhoneNo(@NonNull String phoneNo) {this.phoneNo = phoneNo;}
+
     private String profilePicture;
     private String city;
     private String state;
     private String country;
+
 
     public String getName(String fName, String lName)
     {
@@ -47,7 +59,5 @@ public class User
 
     @DBRef
     private List<Courses> courses = new ArrayList<>();
-
-
 
 }
