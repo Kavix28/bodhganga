c5608cfd8f90076ebfa59910b45b0cbeb54e1c49
codes-by-utkarsh <usrivastava2023@gmail.com>
2026-02-04 00:58:04 +0530

    Connecting the basic User Workflow connecting with DTO

---
+++ backend/src/main/java/com/bodhganga/bodhganga/dto/ApiResponseDTO.java
@@ -0,0 +1,16 @@
+package com.bodhganga.bodhganga.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponseDTO {
+    private boolean success;
+    private String message;
+    private Object data;
+}
