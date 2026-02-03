6f97aeb5672a9134ad9df49c1480874422f35d87
codes-by-utkarsh <usrivastava2023@gmail.com>
2026-02-04 01:52:00 +0530

    User basic required RESTapi are made

--- backend/src/main/java/com/bodhganga/bodhganga/controllers/UserController.java
+++ backend/src/main/java/com/bodhganga/bodhganga/controllers/UserController.java
@@ -3,83 +3,70 @@ package com.bodhganga.bodhganga.controllers;
 import java.util.List;
 
 import com.bodhganga.bodhganga.entity.User;
-import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
-import com.bodhganga.bodhganga.entity.Courses;
 import com.bodhganga.bodhganga.service.UserServices;
 
-@Component
 @RestController
 @RequestMapping("/profile")
-public class UserController
-{
+public class UserController {
     @Autowired
     private UserServices userServices;
 
     @GetMapping
-    public List<User> getUser()
-    {
+    public List<User> getUser() {
         return userServices.getAll();
     }
 
     @PostMapping
-    public void createUser(@RequestBody User user)
-    {
+    public void createUser(@RequestBody User user) {
         userServices.saveUser(user);
     }
 
     @PutMapping
-    public void updateUser(@RequestBody User user) //to update user has to enter email and password. we will first check the credentials and then find the email add in db and update details.
-    {
+    public void updateUser(@RequestBody User user) {
         User userinDb = userServices.findByEmail(user.getEmail());
-
+        // TODO: Implement update logic
     }
 
+    // TODO: Fix these methods - they have compilation errors
+    /*
+     * @GetMapping()
+     * public String getProfile()
+     * {
+     * return user.getName();
+     * }
+     * 
+     * @GetMapping("/my-courses")
+     * public List<Courses> getCourses()
+     * {
+     * try{
+     * return userServices.getCourses();
+     * } catch (Exception e) {
+     * throw new RuntimeException("Internal Server Error");
+     * }
+     * }
+     * 
+     * @PostMapping("/edit-profile")
+     * public void editProfile()
+     * {
+     * return userServices.editProfile();
+     * }
+     */
 
-
-    @GetMapping()
-    public String getProfile()
-    {
-        return user.getName();
-    }
-    @GetMapping("/my-courses")
-    public List<Courses> getCourses()
-    {
-        try{
-            return userServices.getCourses();
-        } catch (Exception e) {
-            throw new RuntimeException("Internal Server Error");
-        }
-
-    }
     @GetMapping("/edit-profile")
-    public String getEditProfile()
-    {
-        return "";
+    public String getEditProfile() {
+        return "Edit profile page";
     }
 
-    @PostMapping("/edit-profile")
-    public void editProfile()
-    {
-        return userServices.editProfile();
-    }
-
-
-    public String postEditProfile()
-    {
-        return "";
-    }
     @GetMapping("/account-security")
-    public String getAccountSecurity()
-    {
-        return "";
+    public String getAccountSecurity() {
+        return "Account security page";
     }
+
     @GetMapping("/payment")
-    public String getPayment()
-    {
-        return "";
+    public String getPayment() {
+        return "Payment page";
     }
 }
